050626e899bb91ac6adc900894bd05478ca4de68
amico <amico8amico@gmail.com>
2016-08-31 15:33:54 +0800

    一覧表示機能を実装

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -19,6 +19,23 @@
     $stmt->execute($data);
   }
 
+  // データの一覧表示
+  $sql = 'SELECT * FROM `posts` ORDER BY `created` DESC';
+  // SQLを実行
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+
+  while (1) {
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($rec == false) {
+      break;
+    }
+    echo $rec['nickname'];
+    echo $rec['comment'];
+    echo $rec['created'];
+    echo '<br>';
+  }
+
   // ③DB切断
   $dbh = null;
 ?>
