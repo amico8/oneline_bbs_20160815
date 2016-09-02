a5eff3fd8185ea5e8d916e4240f7688e4f7d33e3
amico <amico8amico@gmail.com>
2016-09-02 14:08:42 +0800

    編集機能の実装

--- bbs.php
+++ bbs.php
@@ -7,12 +7,42 @@
   $dbh = new PDO($dsn, $user, $password);
   $dbh->query('SET NAMES utf8');
 
+  // ------------------------
+  // 歯車アイコンクリック時
+  $editName = '';
+  $editComment = '';
+  $id = '';
+  if (!empty($_GET['action']) && $_GET['action'] == 'edit') {
+    $sql = 'SELECT * FROM `posts` WHERE `id` = ?';
+    $data[] = $_GET['id'];
+
+    // SQL実行
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+
+    // データを取得
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    // 値を変数に格納
+    $editName = $rec['nickname'];
+    $editComment = $rec['comment'];
+    $id = $rec['id'];
+  }
+
   // POST送信された時のみ登録処理を実行
   if (!empty($_POST)) {
-    // ②SQL文を作成
-    $sql = 'INSERT INTO `posts`(`nickname`, `comment`, `created`) VALUES (?, ?, now())';
-    $data[] = $_POST['nickname'];
-    $data[] = $_POST['comment'];
+    if (!empty($_POST['id'])) {
+      // データを更新する
+      $sql = 'UPDATE `posts` SET `nickname`=?,`comment`=? WHERE `id` = ?';
+      $data[] = $_POST['nickname'];
+      $data[] = $_POST['comment'];
+      $data[] = $_POST['id'];
+
+    } else {
+      // データを登録する
+      $sql = 'INSERT INTO `posts`(`nickname`, `comment`, `created`) VALUES (?, ?, now())';
+      $data[] = $_POST['nickname'];
+      $data[] = $_POST['comment'];
+    }
 
     // SQLを実行
     $stmt = $dbh->prepare($sql);
@@ -89,19 +119,24 @@
           <!-- nickname -->
           <div class="form-group">
             <div class="input-group">
-              <input type="text" name="nickname" class="form-control" id="validate-text" placeholder="nickname" required>
+              <input type="text" name="nickname" class="form-control" id="validate-text" placeholder="nickname" required value="<?php echo $editName; ?>">
               <span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
             </div>
           </div>
           <!-- comment -->
           <div class="form-group">
             <div class="input-group" data-validate="length" data-length="4">
-              <textarea type="text" class="form-control" name="comment" id="validate-length" placeholder="comment" required></textarea>
+              <textarea type="text" class="form-control" name="comment" id="validate-length" placeholder="comment" required><?php echo $editComment; ?></textarea>
               <span class="input-group-addon danger"><span class="glyphicon glyphicon-remove"></span></span>
             </div>
           </div>
           <!-- つぶやくボタン -->
-          <button type="submit" class="btn btn-primary col-xs-12" disabled>つぶやく</button>
+          <?php if (!empty($_GET['action']) && $_GET['action'] == 'edit'): ?>
+            <button type="submit" class="btn btn-primary col-xs-12" >更新する</button>
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
+          <?php else: ?>
+            <button type="submit" class="btn btn-primary col-xs-12" >つぶやく</button>
+          <?php endif; ?>
         </form>
       </div>
 
@@ -111,10 +146,12 @@
           <?php foreach($data as $d): ?>
           <article class="timeline-entry">
               <div class="timeline-entry-inner">
-                  <div class="timeline-icon bg-success">
-                      <i class="entypo-feather"></i>
-                      <i class="fa fa-cogs"></i>
-                  </div>
+                  <a href="bbs.php?action=edit&id=<?php echo $d['id']; ?>">
+                    <div class="timeline-icon bg-success">
+                        <i class="entypo-feather"></i>
+                        <i class="fa fa-cogs"></i>
+                    </div>
+                  </a>
                   <div class="timeline-label">
                     <?php
                       // 1. 文字列型から日付型へ変換する
@@ -146,7 +183,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <!-- Include all compiled plugins (below), or include individual files as needed -->
   <script src="assets/js/bootstrap.js"></script>
-  <script src="assets/js/form.js"></script>
+  <!-- <script src="assets/js/form.js"></script> -->
 </body>
 </html>
 
